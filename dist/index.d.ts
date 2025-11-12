43ccd08d4e2cef27c2955f0fb15218dcf95527b6
seony choe <csh99240@naver.com>
2025-11-12 20:35:10 +0900

    chore: dist 파일 삭제

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface Props {
-    hasNextPage: Boolean;
-    fetchNextPage: () => void;
-    threshold?: number;
-}
-declare const useIntersectionObserver: ({ hasNextPage, fetchNextPage, threshold, }: Props) => import("react").MutableRefObject<HTMLElement | null>;
-export default useIntersectionObserver;
